2b51772d5e4d8c07afe6f23887f3a437313ad4e4
TannerDryden <tdd7197@gmail.com>
2021-02-10 14:24:20 -0800

    Project finished

---
+++ BinaryTree.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    public class BinaryTree
+    {
+        // Accessor functions
+        public void TreeInsert(int key)
+        {
+            this.root = this.InsertRecursive(this.root, key);
+        }
+
+        public void InorderTraversal()
+        {
+            this.InOrderTraversalRecursive(this.root);
+        }
+
+        public int TreeQuantity()
+        {
+            return this.GetCount();
+        }
+
+        public int TheoreticalMinLevels()
+        {
+            return this.TheoreticalMinLevelsCalc();
+        }
+
+        public int ActualLevels()
+        {
+            return this.ActualLevelsCalc(this.root);
+        }
+
+        // A recursive function that inserts a new tree
+        Node InsertRecursive(Node root, int key)
+        {
+            // Return a new node if tree is empty
+            if (root == null)
+            {
+                root = new Node(key);
+                return root;
+            }
+
+            // Case for duplicates
+            if (key != root.GetKey())
+            {
+                // Recur down the tree
+                if (key < root.GetKey())
+                {
+                    root.SetLeft(this.InsertRecursive(root.GetLeft(), key));
+                }
+                else if (key > root.GetKey())
+                {
+                    root.SetRight(this.InsertRecursive(root.GetRight(), key));
+                }
+            }
+
+            // Return the node
+            return root;
+        }
+
+        // Node/Level counter
+        private int count = 0;
+        private int theoreticalLevels = 0;
+
+        private int AddCount() => this.count += 1;
+
+        private int GetCount() => this.count;
+
+        // Level calculators
+        private int TheoreticalMinLevelsCalc()
+        {
+           this.theoreticalLevels += (int)Math.Ceiling(Math.Log2(this.count + 1));
+           return this.theoreticalLevels;
+        }
+
+        private int ActualLevelsCalc(Node node)
+        {
+            if (node == null)
+            {
+                return 0;
+            }
+
+            return Math.Max(this.ActualLevelsCalc(node.GetLeft()), this.ActualLevelsCalc(node.GetRight())) + 1;
+
+        }
+
+        // Root of the tree
+        private Node root;
+
+        private class Node
+        {
+            // initializers
+            private int key;
+            private Node left;
+            private Node right;
+
+            // Constructor
+            public Node(int newKey)
+            {
+                this.SetKey(newKey);
+                this.SetLeft(null);
+                this.SetRight(null);
+            }
+
+            // Get functions
+            public int GetKey() => this.key;
+
+            public Node GetLeft() => this.left;
+
+            public Node GetRight() => this.right;
+
+            // Set functions
+            public int SetKey(int newKey) => this.key = newKey;
+
+            public Node SetLeft(Node newLeft) => this.left = newLeft;
+
+            public Node SetRight(Node newRight) => this.right = newRight;
+        }
+
+        // inorder traversal function
+        private void InOrderTraversalRecursive(Node node)
+        {
+            if (node != null)
+            {
+                this.InOrderTraversalRecursive(node.GetLeft());
+                Console.Write(node.GetKey() + " ");
+                this.AddCount();
+                this.InOrderTraversalRecursive(node.GetRight());
+            }
+        }
+    }
+}
---
+++ OrderNumbers.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Name:    Tanner Dryden
+ * Class:   CptS 321
+ * Prof:    Venera Arnaoudova
+ *          Homework 1
+ *          Description:    This program utilizes a BinaryTree class that takes numbers from user input
+ *                          and parses the data into a binary tree omitting duplicate data and outputs
+ *                          the data in order. The program also gives statistics of the tree created like
+ *                          theoretical number of levels in the tree, actual levels, and quantity of data
+ *                          entered into the tree.
+ */
+
+using System;
+using Tree;
+
+namespace HW1
+{
+    class OrderNumbers
+    {
+        public static void Main()
+        {
+            // Prompt user
+            Console.WriteLine("Enter numbers between [0-100] that are separated by spaces:");
+
+            // Takes user input
+            string userInput;
+            userInput = Console.ReadLine();
+
+            // Parsing userInput into substrings
+            string[] listOfStrings = userInput.Split(' ');
+
+            // Instantiate the tree
+            BinaryTree numTree = new BinaryTree();
+
+            // Load the substrings into the tree
+            foreach (var substring in listOfStrings)
+            {
+                numTree.TreeInsert(Convert.ToInt32(substring));
+            }
+
+            // Output to console
+            Console.WriteLine("Your assorted string is: ");
+
+            // Traverse the tree and print substrings in order
+            numTree.InorderTraversal();
+
+            // Print quantity of numbers in the tree
+            Console.WriteLine("\nThe number of nodes in the tree is: " + numTree.TreeQuantity());
+
+            // Print theoretical min number of levels in the tree
+            Console.WriteLine("The theoretical minimum number of levels in the tree is: " + numTree.TheoreticalMinLevels());
+
+            // Print actual number of levels in the tree
+            Console.WriteLine("The actual number of levels in the tree is: " + numTree.ActualLevels());
+        }
+    }
+}
